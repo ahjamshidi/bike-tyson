05fa55dba1cdb4d350ab6e9fe6af6aad4dd7c4a6
ahjamshidi <a.hossein.jamshidi@gmail.com>
2024-06-28 10:24:36 +0200

    activate bottom menu

--- src/components/appBottomNavigation/AppBottomNavigation.tsx
+++ src/components/appBottomNavigation/AppBottomNavigation.tsx
@@ -6,27 +6,41 @@ import FmdGoodRoundedIcon from '@mui/icons-material/FmdGoodRounded';
 import DirectionsBikeRoundedIcon from '@mui/icons-material/DirectionsBikeRounded';
 import ListAltRoundedIcon from '@mui/icons-material/ListAltRounded';
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import { useLocation, useNavigate } from 'react-router-dom';
 export default function AppBottomNavigation() {
-  const [value, setValue] = React.useState(0);
-
+  const navigate = useNavigate();
+  const location = useLocation();
+  const initpath = ['/','/MyBikesPage'].includes(location.pathname)?location.pathname:'';
+  const [value, setValue] = React.useState(initpath);
   return (
-    <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 ,zIndex:2}}>
+    <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, zIndex: 2 }}>
       <BottomNavigation
         showLabels
         value={value}
         onChange={(_event, newValue) => {
           setValue(newValue);
+          navigate(newValue, { replace: true });
         }}
       >
-        <BottomNavigationAction label="Explore" icon={<FmdGoodRoundedIcon />} />
+        <BottomNavigationAction
+          label="Explore"
+          icon={<FmdGoodRoundedIcon />}
+          value={'/'}
+        />
         <BottomNavigationAction
           label="Bikes"
           icon={<DirectionsBikeRoundedIcon />}
+          value={'/MyBikesPage'}
         />
-        <BottomNavigationAction label="Reports" icon={<ListAltRoundedIcon />} />
+        <BottomNavigationAction
+          label="Reports"
+          icon={<ListAltRoundedIcon />}
+          value={'/UserReportsPage'}
+        />
         <BottomNavigationAction
           label="Profile"
           icon={<AccountCircleRoundedIcon />}
+          value={'/editUser'}
         />
       </BottomNavigation>
     </Box>
