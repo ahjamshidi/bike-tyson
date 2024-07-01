557ebecf4026e393b6a26a91a79e8e5df99d32ea
ahjamshidi <a.hossein.jamshidi@gmail.com>
2024-07-01 17:10:33 +0200

    fix login problems

--- src/components/googleLogin/GoogleLogin.tsx
+++ src/components/googleLogin/GoogleLogin.tsx
@@ -3,6 +3,7 @@ import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
 import { Box } from '@mui/material';
 import { CONFIG } from '@/constances/config';
+import { jwtDecode } from 'jwt-decode';
 
 const GoogleLoginComponent: React.FC = () => {
   const clientId = process.env.GOOGLE_CLIENT_ID || '';
@@ -24,6 +25,8 @@ const GoogleLoginComponent: React.FC = () => {
       if (data.token) {
         localStorage.setItem('isVisited', 'true'); // visited app true
         localStorage.setItem('jwt', data.token); // Store the token
+        const decodedToken: any = jwtDecode(data.token); // Decode the token
+        localStorage.setItem('user_id', decodedToken.userId); //Store user_id
         navigate('/'); // Redirect to the home page or any protected route
       } else {
         console.error('Login failed: No token received');
